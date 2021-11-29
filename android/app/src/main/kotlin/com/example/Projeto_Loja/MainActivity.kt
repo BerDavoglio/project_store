eaf12f607788370d301c16f44adf7befa9ad8514
berdavoglio <berdavoglio@gmail.com>
2021-11-29 00:50:14 -0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/Projeto_Loja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Projeto_Loja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const BASE_API_URL = 'https://flutter-cod3r-projeto-loja-247-default-rtdb.firebaseio.com/';
+}
